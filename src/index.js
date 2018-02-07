ab8db7d4bdddf66e991b5f0e57b6c81c0a6bf25a
Jane Wang <jw9billions@gmail.com>
2018-02-06 16:16:26 -0800

    make it compile with Alexa Skill Kit in Amazon

--- src/index.js
+++ src/index.js
@@ -2,3 +2,8 @@
 
 const Alexa = require('alexa-sdk');
 const questions = require('./question');
+
+const APP_ID = 'amzn1.ask.skill.'; //could not generate id because of slot types
+
+exports.handler = function (event, context) {  
+};
